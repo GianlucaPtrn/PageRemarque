228ead54062c01fe14f28f374916c8b96ebedfce
GianlucaPtrn <200188@site.asty-moulin.be>
2023-03-21 15:55:13 +0100

    add controller modif remarque

---
+++ controller/modifRemarqueController.php
@@ -0,0 +1,6 @@
+<?php
+    $uri = $_SERVER['REQUEST_URI'];
+    if($uri == "/DigitSchool_TFE_2023/index.php?/templates/remarque/modifRemarque"){
+        require_once "templates/remarque/modifRemarque.php";
+    }
+?> 
